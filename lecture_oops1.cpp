3d2017c399543f4b33ce2540314ca27a6361ee7d
anonymous-oracle <suhass149@gmail.com>
2020-07-21 17:53:51 +0530

    update

--- lecture_oops1.cpp
+++ lecture_oops1.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 
 class Player{
-
+public:
 // attributes
 string name;
 int health;
 int xp;
 
 // methods
-void talk(string speech);
-bool is_dead();
+void talk(string speech) {cout << name <<" says: " << speech << endl;}
+bool is_dead() {return this->health > 0? 0 : 1 ; }
 };
 
 
-
+class Account{
+    public:
+    // attributes
+   string name;
+   double balance;
+   
+   // methods
+   bool withdraw(double amt);
+   bool deposit(double amt);
+};
 
 
 
@@ -41,6 +50,11 @@ int main(int argc, char const *argv[])
     Player frank, hero;
     Player *enemy;
     enemy = new Player;
+    frank.health = 100;
+    frank.xp = 1000;
+    frank.talk("my name is frank!");
+    if (frank.is_dead()){cout << "frank is dead"<<endl;}
+    else {cout << "frank is alive"<<endl;}
     Player players[] {frank, hero};
     vector <Player> player_vec {frank, hero, *enemy};
     player_vec.push_back(Player());
